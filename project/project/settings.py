f20913e8c3de0f1539b3147560a48e66948d3a06
mahdi hadian <mahdihadian@hotmail.com>
2020-08-23 09:34:32 +0430

    all good before data relations

--- project/project/settings.py
+++ project/project/settings.py
@@ -71,24 +71,24 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'spoty',
-        'USER': 'admin',
-        'PASSWORD': '123456',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'spoty',
+#         'USER': 'admin',
+#         'PASSWORD': '123456',
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
--- project/spots/models.py
+++ project/spots/models.py
@@ -2,45 +2,44 @@ from django.db import models
 
 
 # Create your models here.
-# TODO
+class User(models.Model):
+    name = models.TextField()
+    email = models.TextField(unique=True)
+    token = models.TextField(unique=True)
+    created_at = models.DateField(blank=True, null=True, auto_created=True, auto_now=True)
 
+    # spots = models.ForeignKey('spot', models.DO_NOTHING, null=True, db_column='spots', related_name='+')
+    # followings = models.ForeignKey('self', models.DO_NOTHING, null=True, db_column='followings', related_name='+')
 
-class SavedSpot(models.Model):
-    spots = models.ForeignKey('Spot', models.DO_NOTHING, db_column='spots', related_name='+')
+    # class Meta:
+    #     managed = False
+    #     db_table = 'user'
 
-    class Meta:
-        managed = False
-        db_table = 'saved_spot'
+    def __str__(self):
+        return self.name
 
 
 class Spot(models.Model):
     latitude = models.TextField()
     longitude = models.TextField()
-    user = models.ForeignKey('User', models.DO_NOTHING, db_column='user', related_name='+')
-    description = models.TextField()
-    likes = models.IntegerField()
-    dislikes = models.IntegerField()
-    comments = models.IntegerField()
+    description = models.TextField(null=True, default="")
+    likes = models.IntegerField(null=True, default=0)
+    dislikes = models.IntegerField(null=True, default=0)
+    comments = models.IntegerField(null=True, default=0)
 
-    class Meta:
-        managed = False
-        db_table = 'spot'
+    # user = models.ForeignKey('User', models.DO_NOTHING, db_column='user', related_name='+')
 
-    def __str__(self):
-        return "{} {},{}".format(self.user.name, self.latitude, self.longitude)
-
-
-class User(models.Model):
-    name = models.TextField()
-    email = models.TextField()
-    token = models.TextField()
-    created_at = models.DateField(blank=True, null=True, auto_created=True, auto_now=True)
-    spots = models.ForeignKey('spot', models.DO_NOTHING, null=True, db_column='spots', related_name='+')
-    followings = models.ForeignKey('self', models.DO_NOTHING, null=True, db_column='followings', related_name='+')
-
-    class Meta:
-        managed = False
-        db_table = 'user'
+    # class Meta:
+    #     managed = False
+    #     db_table = 'spot'
 
     def __str__(self):
-        return self.name
+        return "{},{}".format(self.latitude, self.longitude)
+        # return "{} {},{}".format(self.user.name, self.latitude, self.longitude)
+
+# class SavedSpot(models.Model):
+#     spots = models.ForeignKey('Spot', models.DO_NOTHING, db_column='spots', related_name='+')
+#
+#     class Meta:
+#         managed = False
+#         db_table = 'saved_spot'
